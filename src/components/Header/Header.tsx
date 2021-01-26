97c098560173596e0ce26bb6c378e285b730fdd2
Emil <emil.varn@gmail.com>
2021-01-26 13:30:08 +0200

    Loodud struktuur

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './Header.module.css'
+
+export const Header = () => {
+    return (
+        <div className={classes.header}>
+            header
+        </div>
+    )
+}
+
+
