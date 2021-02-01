ae75776e5c460e2c5d9f2423bed11743cb0ff92d
Steven Klaos <steven.klaos@khk.ee>
2021-02-01 19:51:04 +0200

    Pealehe muudatused

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import classes from './Header.module.css';
 import logo from '../../img/RebuildLogo.png'
 import {NavLink} from "react-router-dom";
--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import classes from './Main.module.css';
-import kusOnOtsing from '../../img/kusOnOtsing.png'
+import kusOnOtsing from '../../img/kusOnOtsing.png';
+import card1 from '../../img/card1.png';
+import card2 from '../../img/card2.png';
+import card3 from '../../img/card3.png';
 import {Button, TextField} from "@material-ui/core";
 
 export const Main = () => {
@@ -19,7 +22,7 @@ export const Main = () => {
                     <Button>Kuidas see töötab &gt;</Button>
                     {/*<span>Kuidas see töötab</span>*/}
                 </div>
-                <div>
+                <div className={classes.searchImg}>
                     <img src={kusOnOtsing} alt="kusOnOtsing"/>
                 </div>
                 <div className={classes.search}>
@@ -28,9 +31,36 @@ export const Main = () => {
             </div>
 
             <div className={classes.cards}>
-                <div className={classes.card}>1</div>
-                <div className={classes.card}>2</div>
-                <div className={classes.card}>3</div>
+                <div className={classes.card}>
+                    <div className={classes.cardChild}>
+                        <img src={card1} alt="card1"/>
+                    </div>
+                    <div className={classes.cardChild}>
+                        <h1>Hello world</h1>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+                    </div>
+                </div>
+
+                <div className={classes.card}>
+                    <div className={classes.cardChild}>
+                        <h1>Hello world</h1>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+                    </div>
+                    <div className={classes.cardChild}>
+                        <img src={card2} alt="card2"/>
+                    </div>
+                </div>
+
+                <div className={classes.card}>
+                    <div className={classes.cardChild}>
+                        <img src={card3} alt="card3"/>
+                    </div>
+                    <div className={classes.cardChild}>
+                        <h1>Hello world</h1>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+                    </div>
+                </div>
+
             </div>
         </>
     )
