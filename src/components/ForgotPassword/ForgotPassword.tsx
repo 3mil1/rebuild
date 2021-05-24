060c6c2dbedcea599c8f71fe86faeca7b5364b7c
Karl Ojarand <karl@play.ee>
2021-05-24 16:54:10 +0300

    eesti keelsed errorid

--- src/components/ForgotPassword/ForgotPassword.tsx
+++ src/components/ForgotPassword/ForgotPassword.tsx
@@ -62,7 +62,7 @@ export const ForgotPassword = React.memo(function () {
                             control={control}
                             defaultValue=""
                             rules={{
-                                required: 'Required',
+                                required: 'Kohustuslik',
                                 pattern: {
                                     value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
                                     message: 'invalid email address'
